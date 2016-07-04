872e9f820ad271b73916a0c8efc08dde0ab5b9d1
zigapk <ziga.patacko@gmail.com>
2016-07-04 13:39:53 +0200

    change main design (colors, remove action bar, etc.)

--- app/src/main/java/com/poviolabs/gotlaid/MainActivity.java
+++ app/src/main/java/com/poviolabs/gotlaid/MainActivity.java
@@ -6,7 +6,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,8 +21,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
         mViewPager = (ViewPager) findViewById(R.id.container);
